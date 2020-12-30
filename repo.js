06f541237863d4148f913657bcd195895f6bccce
Matheus sampaio Queiroga <srherobrine20@gmail.com>
2020-12-30 19:30:34 -0300

    push

--- repo.js
+++ repo.js
@@ -1,15 +1,11 @@
 var exec = require('child_process').exec;
 const core = require('@actions/core');
-// TIME
 const time = (new Date()).toTimeString();
 core.setOutput("time", time);
-// TIME
-// console.log(`Process ${process.cwd()},\n Dirname ${__dirname}\n\n\n`)
-var command = `chmod 777 ${__dirname}/src/repo.sh && ${__dirname}/src/repo.sh`
-var serverstated = exec(command, {
-    detached: false,
-    shell: true,
-    maxBuffer: Infinity});
+// -
+// --
+// ---
+var serverstated = exec(`chmod 777 ${__dirname}/src/repo.sh && ${__dirname}/src/repo.sh`, {detached: false, maxBuffer: Infinity});
 function logoutpu(dados){
     console.log(dados)
 }
@@ -17,9 +13,5 @@ serverstated.stdout.on('data', function (data) {
     logoutpu(data)
 });
 serverstated.on('exit', function (code) {
-    if (code == 0) {
-        let nuLLL = '';
-    } else {
-        core.setFailed('Error code: ' + code);
-    }
+    if (!code == 0) {core.setFailed('Error code: ' + code);};
 });
--- setup.js
+++ setup.js
@@ -1,36 +1,12 @@
 var exec = require('child_process').exec;
 function logoutpu(dados){
     console.log(dados)
-}
-function installAPT(){
-    const core = require('@actions/core');
-    const time = (new Date()).toTimeString();
-    core.setOutput("time", time);
-    // Time End
-    var aptrepo = exec(`bash ${__dirname}/src/setup.sh`, {maxBuffer: Infinity});   
-    aptrepo.stdout.on('data', function (data) {
-        logoutpu(data);
-    });
-    aptrepo.on('exit', function (code) {
-        if (code == 0) {
-            // console.log('Apt Packages Install Sucess')
-            // console.log('Alt sucess')
-            let NULLLL = '';
-        } else {
-            core.setFailed('exit with code: '+code);
-        }
-    });
-}
-// console.log(`Process ${process.cwd()},\n Dirname ${__dirname}\n\n\n`)
+};
 var npmi = exec(`cd ${__dirname} && pwd && npm install`, {detached: false,shell: true});
 console.log('Node Modules install')
-// npmi.stdout.on('data', function (data) {
-//     console.log(data)
-// });
 npmi.on('exit', function (code) {
     if (code == 0) {
         logoutpu('NPM install Sucess')
-        installAPT();
     } else {
         console.log('npm left with an error, code: '+code);
     }
