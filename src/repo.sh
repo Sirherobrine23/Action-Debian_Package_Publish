e4243eb5278bc45bbea0368860fc7703bf8c8dd9
Matheus sampaio Queiroga <srherobrine20@gmail.com>
2021-01-02 18:37:51 -0300

    Update repo.sh

--- src/repo.sh
+++ src/repo.sh
@@ -21,20 +21,25 @@ git config --global user.email "github-actions[bot]@users.noreply.github.com"
 
 git clone $repo -b ${INPUT_BRANCH} $DIR_PATH/repo
 cd $DIR_PATH/repo/
-if ! [ -d ${INPUT_REPO_PATH} ];then
+if [ -d ${INPUT_REPO_PATH} ];then
+    cd ${INPUT_REPO_PATH}
+    pwd
+    cp -rfv ${INPUT_PATH} ./
+    git add .
+    git commit -m 'Upload Package, Github Actions' -m "Package Path Uploaded: ${INPUT_PATH}"
+    if [ $INPUT_SQUASH == 'true' ];then
+        echo "This will erase the file history"
+        git rebase --root --autosquash
+        git commit -m 'squash Files' -m "Package Path Uploaded: ${INPUT_PATH}" -m 'Git Squash'
+    fi
+    if git push --force --verbose ;then
+        echo
+    else
+        echo "Erro in "
+        exit 3
+    fi
+else
     exit 2
 fi
-cd ${INPUT_REPO_PATH}
-pwd
-cp -rfv ${INPUT_PATH} ./
-git add .
-git commit -m 'Upload Package, Github Actions' -m "Package Path Uploaded: ${INPUT_PATH}"
-if [ $INPUT_SQUASH == 'true' ];then
-    echo "This will erase the file history"
-    git rebase --root --autosquash
-    git commit -m 'squash Files' -m "Package Path Uploaded: ${INPUT_PATH}" -m 'Git Squash'
-fi
-if ! git push --force --verbose ;then
-    echo "Erro in "
-    exit 3
+
 exit 0
